5511cd914517704ba54e5d533b7804578733519b
yuichi <ishideyuichi@yahoo.co.jp>
2020-05-07 18:20:01 +0900

    Add: ユーザー認証機能とユーザーページを追加 deviseを導入し、ユーザー認証機能とユーザーページを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_095611) do
+ActiveRecord::Schema.define(version: 2020_05_06_043811) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,22 @@ ActiveRecord::Schema.define(version: 2020_04_22_095611) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
     t.string "picture"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "address"
+    t.text "introduction"
+    t.integer "postcode"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
